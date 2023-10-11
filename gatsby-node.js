d8ad5c477ce7a09408d4373fe8c10bbf1fda0e19
parthdasawant <parthdasawant07@gmail.com>
2023-10-11 15:12:21 +0530

    with setmax with proper way

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,82 +1,74 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
 const path = require('path');
 const _ = require('lodash');
-
-const EventEmitter = require('events');
-
-// Create a new EventEmitter instance
-const myEmitter = new EventEmitter();
-
-// Increase the listener limit for this EventEmitter
-myEmitter.setMaxListeners(15);
+const { create } = require('domain'); // Import the EventEmitter module
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
-  const postTemplate = path.resolve(`src/templates/post.js`);
-  const tagTemplate = path.resolve('src/templates/tag.js');
 
-  const result = await graphql(`
-    {
-      postsRemark: allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/content/posts/" } }
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              slug
+  try {
+    // Create a new EventEmitter instance and increase the listener limit
+    const myEmitter = new create();
+    myEmitter.setMaxListeners(15);
+
+    const result = await graphql(`
+      {
+        postsRemark: allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "/content/posts/" } }
+          sort: { order: DESC, fields: [frontmatter___date] }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
             }
           }
         }
-      }
-      tagsGroup: allMarkdownRemark(limit: 2000) {
-        group(field: frontmatter___tags) {
-          fieldValue
+        tagsGroup: allMarkdownRemark(limit: 2000) {
+          group(field: frontmatter___tags) {
+            fieldValue
+          }
         }
       }
+    `);
+
+    if (result.errors) {
+      reporter.panicOnBuild(`Error while running GraphQL query: ${result.errors}`);
+      return;
     }
-  `);
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`);
-    return;
+    // Create post detail pages
+    const postTemplate = path.resolve(`src/templates/post.js`);
+    const tagTemplate = path.resolve('src/templates/tag.js');
+    const posts = result.data.postsRemark.edges;
+
+    posts.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.slug,
+        component: postTemplate,
+        context: {},
+      });
+    });
+
+    // Extract tag data from query
+    const tags = result.data.tagsGroup.group;
+    // Make tag pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/pensieve/tags/${_.kebabCase(tag.fieldValue)}/`,
+        component: tagTemplate,
+        context: {
+          tag: tag.fieldValue,
+        },
+      });
+    });
+  } catch (error) {
+    reporter.panicOnBuild(`Error while creating pages: ${error}`);
   }
-
-  // Create post detail pages
-  const posts = result.data.postsRemark.edges;
-
-  posts.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.slug,
-      component: postTemplate,
-      context: {},
-    });
-  });
-
-  // Extract tag data from query
-  const tags = result.data.tagsGroup.group;
-  // Make tag pages
-  tags.forEach(tag => {
-    createPage({
-      path: `/pensieve/tags/${_.kebabCase(tag.fieldValue)}/`,
-      component: tagTemplate,
-      context: {
-        tag: tag.fieldValue,
-      },
-    });
-  });
 };
 
-// https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  // https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
   if (stage === 'build-html' || stage === 'develop-html') {
     actions.setWebpackConfig({
       module: {
@@ -106,7 +98,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
         '@fonts': path.resolve(__dirname, 'src/fonts'),
         '@hooks': path.resolve(__dirname, 'src/hooks'),
         '@images': path.resolve(__dirname, 'src/images'),
-        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@pages': path.resolve(__dirname, 'src.pages'),
         '@styles': path.resolve(__dirname, 'src/styles'),
         '@utils': path.resolve(__dirname, 'src/utils'),
       },
